a5eebfda68df59ab9db57e2fa7fe288e0e5e1856
razvanpotoceanu <razvanpotoceanu4@gmail.com>
2025-12-01 20:52:58 +0200

    Feature: Implement the console application menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,67 @@
 package org.example;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Library library = new Library();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        int choice;
+        do {
+            System.out.println("\n---Meniu Biblioteca---\n");
+            System.out.println("1. Adauga carte");
+            System.out.println("2. Afiseaza toate cartile");
+            System.out.println("3. Imprumuta o carte");
+            System.out.println("4. Inapoiaza o carte");
+            System.out.println("5. Sterge carte");
+            System.out.println("0. Inchide");
+            System.out.println("Alegeti un numar: ...");
+
+            while (!scanner.hasNextInt()) {
+                System.out.print("Try a number: ");
+                scanner.next();
+            }
+
+            choice = scanner.nextInt();
+
+            scanner.nextLine();
+
+            switch(choice){
+                case 1:
+
+                    System.out.println("Introduceti titlul cartii: ");
+                    String title = scanner.nextLine();
+                    System.out.println("Introduceti autorul cartii: ");
+                    String author = scanner.nextLine();
+                    System.out.println("Introduceti ISBN-ul cartii: ");
+                    String ISBN = scanner.nextLine();
+                    Book newBook = new Book(title, author, ISBN, BookStatus.AVAILABLE);
+                    library.addBook(newBook);
+                    break;
+                case 2:
+                    library.displayAllBooks();
+                    break;
+
+                case 3:
+                    System.out.println("Introduceti ISBN-ul cartii: ");
+                    ISBN = scanner.nextLine();
+                    library.borrowBook(ISBN);
+                    break;
+
+                case 4:
+                    System.out.println("Introduceti ISBN-ul cartii: ");
+                    ISBN = scanner.nextLine();
+                    library.returnBook(ISBN);
+                    break;
+
+                case 5:
+                    System.out.println("Introduceti ISBN-ul cartii: ");
+                    ISBN = scanner.nextLine();
+                    library.removeBookByISBN(ISBN);
+                    break;
+
+            }
+        }while(choice != 0);
     }
 }
